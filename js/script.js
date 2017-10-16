f018319c979b61e5c0541d922ddeea38c4a9c0a1
sweetsourire <bkim1289@gmail.com>
2017-10-16 12:18:24 -0400

    added project color circles

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var html = '';
+var red;
+var green;
+var blue;
+var rgbColor;
+
+function randomRGB() {
+  return Math.floor(Math.random() * 256 );
+}
+
+function randomColor() {
+  var color = 'rgb(';
+  color += randomRGB() + ',';
+  color += randomRGB() + ',';
+  color += randomRGB() + ')';
+  return color;
+}
+
+function print(message){
+  document.write(message);
+}
+
+for (var i = 0; i < 10; i += 1) {
+  rgbColor = randomColor();
+  html += '<div style="background-color:' + rgbColor + '"></div>';
+}
+
+print(html);
+
